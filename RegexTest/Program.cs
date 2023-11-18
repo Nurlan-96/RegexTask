ede53505e581ac08dd1a89f8bb5944803881c402
Nurlan <nurlanea@code.edu.az>
2023-11-18 23:42:41 +0400

    first

---
+++ RegexTest/Program.cs
@@ -0,0 +1,6 @@
+﻿using System.Text.RegularExpressions;
+string pattern = @"^\w[\w-]*@[\w]+(\.[\w]+)+";
+Console.WriteLine(Regex.IsMatch("nurlan@gmail.com",pattern));
+Console.WriteLine(Regex.IsMatch("nurlan@code.edu.az", pattern));
+Console.WriteLine(Regex.IsMatch("nur-lan@code.edu.az", pattern));
+Console.WriteLine(Regex.IsMatch("-nurlan@code.edu.az", pattern)); // bu false olmalidi
